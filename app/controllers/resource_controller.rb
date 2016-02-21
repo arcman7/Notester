8f87c1bbe211e55c256ee69940a5cb7857f941d6
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-02-21 01:30:45 -0800

    add fix resource route if block

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -3,19 +3,17 @@ class ResourceController < ApplicationController
 
   end
 
-  def create
-    @new_resource = Resource.new(params.require(:resource).permit(:title, :description, :file)
-    if params[:user]
-      @user = User.find(params[:user])
-    end
-    begin
-      @new_resource.save
-      @user.resources << @new_resource
-    rescue ActiveRecord::RecordInvalid => e
-      render json: {error: e.record.errors.details}, status: 400
-    end
-    render nothing: true, status: 204
-  end
-
-
+  # def create
+  #   @new_resource = Resource.new(params.require(:resource).permit(:title, :description, :file)
+  #   if params[:user]
+  #     @user = User.find(params[:user])
+  #   end
+  #   begin
+  #     @new_resource.save
+  #     @user.resources << @new_resource
+  #   rescue ActiveRecord::RecordInvalid => e
+  #     render json: {error: e.record.errors.details}, status: 400
+  #   end
+  #   render nothing: true, status: 204
+  # end
 end
