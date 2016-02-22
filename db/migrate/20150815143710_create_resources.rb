7f42b45f85d281cc5fea9e6a91be46ebf1b4f9e8
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-02-21 20:21:41 -0800

    add consistent spacing in db files

--- db/migrate/20150815143710_create_resources.rb
+++ db/migrate/20150815143710_create_resources.rb
@@ -1,9 +1,9 @@
 class CreateResources < ActiveRecord::Migration
   def change
     create_table :resources do |t|
-      t.string :title
-      t.text :description
-      t.binary :file
+      t.string     :title
+      t.text       :description
+      t.binary     :file
       t.references :user
 
       t.timestamps
