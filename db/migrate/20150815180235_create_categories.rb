b53cc501177ea0cb5db87786a6448f15419c5d16
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-02-21 18:56:05 -0800

    add text description field to categories

--- db/migrate/20150815180235_create_categories.rb
+++ db/migrate/20150815180235_create_categories.rb
@@ -1,7 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
      create_table :categories  do |t|
-     t.string :name
+     t.string     :name
+     t.text       :description
      t.references :parent_category, index: true
 
      t.timestamps
