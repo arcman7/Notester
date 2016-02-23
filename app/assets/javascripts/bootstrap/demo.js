e63f90cf16a864762a0fc1ee2329b794eeeb22b7
Arturo Perez <VrturoPerez@gmail.com>
2016-02-23 01:32:03 -0800

    add sign in and sign up pages

--- app/assets/javascripts/bootstrap/demo.js
+++ app/assets/javascripts/bootstrap/demo.js
@@ -17,22 +17,22 @@
         },
         {
           element: '#new-note',
-          intro: '<p class="h4 text-uc"><strong>2: Save the note</strong></p><p>Press this button to save your note.</p>',
+          intro: '<p class="h4 text-uc"><strong>2: Start a new note </strong></p><p>Press this button to start a new note.</p>',
           position: 'left'
         },
         {
-          element: '#new-note',
-          intro: '<p class="h4 text-uc"><strong>2: Save the note</strong></p><p>Press this button to save your note.</p>',
+          element: '#save-note',
+          intro: '<p class="h4 text-uc"><strong>3: Save the note</strong></p><p>Press this button to save your note.</p>',
           position: 'left'
         },
         {
           element: '#note-list',
-          intro: '<p class="h4 text-uc"><strong>3: Recent Notes</strong></p><p>See your recent notes being displayed.</p>',
+          intro: '<p class="h4 text-uc"><strong>4: Recent Notes</strong></p><p>See your recent notes being displayed.</p>',
           position: 'left'
         },
         {
           element: '#tree-list',
-          intro: '<p class="h4 text-uc"><strong>4: Note Tree</strong></p><p>Click on a note to see what others are saying!</p>',
+          intro: '<p class="h4 text-uc"><strong>5: Note Tree</strong></p><p>Click on a note to see what others are saying!</p>',
           position: 'right'
         }
       ],
