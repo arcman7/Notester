529f10cbbcd8232faccc716b2d3811f4328879dd
Arturo Perez <VrturoPerez@gmail.com>
2016-02-23 13:47:39 -0800

    add params to user sign up/in forms

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :resource
   resources :welcome, only: [:index]
 
-
+  get 'signup'  => 'users#new'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
