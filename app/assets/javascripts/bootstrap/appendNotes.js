2f28f0534424340ef4c3861e7e75ee4f61c4297a
Arturo Perez <VrturoPerez@gmail.com>
2016-02-25 23:40:10 -0800

    create textarea value listener

--- app/assets/javascripts/bootstrap/appendNotes.js
+++ app/assets/javascripts/bootstrap/appendNotes.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-  // localStorage
-//   var noteTitle = $('#note-title').text();
-// if($('.form-control').keydown()){
-//   console.log('down')
-// }
+  function change(){
+    $('body').on('click', '.form-control', function() {
+      console.log("hey");
+      test();
+    });
+  }
+  change();
+});
 
-})
-
-// Storage {
-// notes-app: "1,2,3",
-// notes-app-1: "{"id":1,"description":"fsadf\nsfsd\nsdfsdfsd\nfsdfsd","date":1456398652964,"name":"fsadf"}",
-// notes-app-2: "{"id":2,"description":"","date":1456453778922}",
-// notes-app-3: "{"id":3,"user":"guy","name":"New note","description":"","date":1456457898557}", notesterUser: "guy"}
+function test(){
+  $('.form-control').on('input', function() {
+    console.log("heyaaa");
+  });
+}
