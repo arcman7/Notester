79c0219e290eaa75516f96bc3b84359629ce4806
Arturo Perez <VrturoPerez@gmail.com>
2016-03-19 15:27:05 -0700

    keep public button blue

---
+++ app/assets/javascripts/bootstrap/switch.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//   $('#pbToggle').click(function() {
+//     $('publicButton').toggle('btn-default');
+//     $('publicButton').toggle('btn-blue');
+//     $('privateButton').toggle('btn-default');
+// });
